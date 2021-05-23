a6c71c0a025528e0d930eaa9454f81c995109377
Buidangco <Buidangco09@gmail.com>
2021-05-23 10:16:41 +0700

    DA MVC

---
+++ DATNMVC/Areas/Area/Controllers/UserController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using DATNMVC.Models;
+using Newtonsoft.Json;
+
+namespace DATNMVC.Areas.Area.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Area/User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Khoas()
+        {
+            List<Khoa> khoas = new List<Khoa>();
+            string apiUrl = "https://localhost:44307/api";
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(apiUrl + string.Format("/Khoas")).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                khoas = JsonConvert.DeserializeObject<List<Khoa>>(data);
+            }
+
+            return PartialView("_PartialMenu", khoas);
+        }
+
+        public ActionResult GetThongTinGiangVien(int Id)
+        {
+            List<ThongTinGiangVien> thongTinGiangViens = new List<ThongTinGiangVien>();
+            string apiUrl = "https://localhost:44307/api";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(apiUrl + string.Format("/ThongTinTheoKhoa?id={0}", Id)).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                thongTinGiangViens = JsonConvert.DeserializeObject<List<ThongTinGiangVien>>(data);
+            }
+
+            List<ChucVu> chucVus = new List<ChucVu>();
+            HttpClient client1 = new HttpClient();
+            HttpResponseMessage response1 = client1.GetAsync(apiUrl + string.Format("/ChucVus")).Result;
+            if (response1.IsSuccessStatusCode)
+            {
+                string data = response1.Content.ReadAsStringAsync().Result;
+                chucVus = JsonConvert.DeserializeObject<List<ChucVu>>(data);
+            }
+            ViewBag.Chucvu = chucVus;
+            return View("Index1", thongTinGiangViens);
+        }
+
+        public ActionResult GetThongTinGiangVien1(int Id)
+        {
+            ThongTinGiangVien thongTin = new ThongTinGiangVien();
+            string apiUrl = "https://localhost:44307/api";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(apiUrl + string.Format("/ThongTinChiTiet?id={0}", Id)).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                thongTin = JsonConvert.DeserializeObject<ThongTinGiangVien>(data);
+            }
+
+            List<DuAn> duAns = new List<DuAn>();
+            HttpResponseMessage response1 = client.GetAsync(apiUrl + string.Format("/ThongTinChiTietDuAn?id={0}", Id)).Result;
+            if (response1.IsSuccessStatusCode)
+            {
+                string data = response1.Content.ReadAsStringAsync().Result;
+                duAns = JsonConvert.DeserializeObject<List<DuAn>>(data);
+            }
+
+            List<QuaTrinhCongTac> quaTrinhCongTacs = new List<QuaTrinhCongTac>();
+            HttpResponseMessage response2 = client.GetAsync(apiUrl + string.Format("/ThongTinChiTietQTCT?id={0}", Id)).Result;
+            if (response2.IsSuccessStatusCode)
+            {
+                string data = response2.Content.ReadAsStringAsync().Result;
+                quaTrinhCongTacs = JsonConvert.DeserializeObject<List<QuaTrinhCongTac>>(data);
+            }
+
+            List<QuaTrinhDaoTao> quaTrinhDaoTaos = new List<QuaTrinhDaoTao>();
+            HttpResponseMessage response3 = client.GetAsync(apiUrl + string.Format("/ThongTinChiTietQTDT?id={0}", Id)).Result;
+            if (response3.IsSuccessStatusCode)
+            {
+                string data = response3.Content.ReadAsStringAsync().Result;
+                quaTrinhDaoTaos = JsonConvert.DeserializeObject<List<QuaTrinhDaoTao>>(data);
+            }
+            List<ChucVu> chucVus = new List<ChucVu>();
+            HttpClient client1 = new HttpClient();
+            HttpResponseMessage response4 = client1.GetAsync(apiUrl + string.Format("/ChucVus")).Result;
+            if (response1.IsSuccessStatusCode)
+            {
+                string data = response4.Content.ReadAsStringAsync().Result;
+                chucVus = JsonConvert.DeserializeObject<List<ChucVu>>(data);
+            }
+
+            ViewBag.Chucvu = chucVus;
+            ViewBag.Duan = duAns;
+            ViewBag.QTCT = quaTrinhCongTacs;
+            ViewBag.QTDT = quaTrinhDaoTaos;
+
+            return View("ChiTiet", thongTin);
+        }
+    }
+}
---
+++ DATNMVC/Controllers/ThongTinGiangVienController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using DATNMVC.Models;
+using Newtonsoft.Json;
+
+namespace DATNMVC.Controllers
+{
+    public class ThongTinGiangVienController : Controller
+    {
+        // GET: ThongTinGiangVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ThongTinGiangViens(string name)
+        {
+            List<ThongTinGiangVien> thongTinGiangViens = new List<ThongTinGiangVien>();
+            string apiUrl = "https://localhost:44307/api";
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(apiUrl + string.Format("/ThongTinGiangViens")).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                thongTinGiangViens = JsonConvert.DeserializeObject<List<ThongTinGiangVien>>(data);
+            }
+
+            return View(thongTinGiangViens);
+        }
+
+        public ActionResult ThongTinDuAn()
+        {
+            List<DuAn> duAns = new List<DuAn>();
+            string apiUrl = "https://localhost:44307/api";
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(apiUrl + string.Format("/DuAns")).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                duAns = JsonConvert.DeserializeObject<List<DuAn>>(data);
+            }
+
+            List<ThongTinGiangVien> thongTinGiangViens = new List<ThongTinGiangVien>();
+            HttpResponseMessage response1 = client.GetAsync(apiUrl + string.Format("/ThongTinGiangViens")).Result;
+            if (response1.IsSuccessStatusCode)
+            {
+                string data = response1.Content.ReadAsStringAsync().Result;
+                thongTinGiangViens = JsonConvert.DeserializeObject<List<ThongTinGiangVien>>(data);
+            }
+
+            ViewBag.ThongTin = thongTinGiangViens;
+
+            return View(duAns);
+        }
+
+        public ActionResult ThongTinQTCT()
+        {
+            List<QuaTrinhCongTac> quaTrinhCongTacs = new List<QuaTrinhCongTac>();
+            string apiUrl = "https://localhost:44307/api";
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(apiUrl + string.Format("/QuaTrinhCongTacs")).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                quaTrinhCongTacs = JsonConvert.DeserializeObject<List<QuaTrinhCongTac>>(data);
+            }
+
+            return View(quaTrinhCongTacs);
+        }
+
+        public ActionResult ThongTinQTDT()
+        {
+            List<QuaTrinhDaoTao> quaTrinhDaoTaos = new List<QuaTrinhDaoTao>();
+            string apiUrl = "https://localhost:44307/api";
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(apiUrl + string.Format("/QuaTrinhDaoTaos")).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                quaTrinhDaoTaos = JsonConvert.DeserializeObject<List<QuaTrinhDaoTao>>(data);
+            }
+
+            return View(quaTrinhDaoTaos);
+        }
+
+        [HttpGet]
+        public ActionResult Create(ThongTinGiangVien model)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddGiangVien(ThongTinGiangVien model)
+        {
+            string apiUrl = "https://localhost:44307/api";
+            string data = JsonConvert.SerializeObject(model);
+            HttpClient client = new HttpClient();
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PostAsync(apiUrl + string.Format("/ThongTinGiangViens"), content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult CreateDuAn()
+        {
+            List<ThongTinGiangVien> thongTinGiangViens = new List<ThongTinGiangVien>();
+            string apiUrl = "https://localhost:44307/api";
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(apiUrl + string.Format("/ThongTinGiangViens")).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                thongTinGiangViens = JsonConvert.DeserializeObject<List<ThongTinGiangVien>>(data);
+            }
+            ViewBag.TypeItemList = new MultiSelectList(thongTinGiangViens, "ID", "hoTen");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddDunAn(DuAn model, List<int> ID)
+        {
+            foreach(var item in ID)
+            {
+                model.idThongTinGiangVien = (byte?)item;
+                string apiUrl = "https://localhost:44307/api";
+                string data = JsonConvert.SerializeObject(model);
+                HttpClient client = new HttpClient();
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = client.PostAsync(apiUrl + string.Format("/DuAns"), content).Result;
+            }    
+            //if (response.IsSuccessStatusCode)
+            //{
+                return RedirectToAction("ThongTinDuAn");
+            //}
+            //return View();
+        }
+
+        [HttpGet]
+        public ActionResult CreateQTCT()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddQTCT(DuAn model)
+        {
+            string apiUrl = "https://localhost:44307/api";
+            string data = JsonConvert.SerializeObject(model);
+            HttpClient client = new HttpClient();
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PostAsync(apiUrl + string.Format("/QuaTrinhCongTacs"), content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ThongTinQTCT");
+            }
+            return View();
+        }
+    }
+}
